9cd752c96b16af50d556b4738b065befd30a0702
ANKUSH SINGH <ankushsingh_se21a13_54@dtu.ac.in>
2023-04-06 13:45:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const { response } = require("express");
  const _ = require('lodash');
+const path =require("path");
 //////////////////////////////////////////////
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -13,12 +14,14 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 //////////////////////////////////////////////
 const app = express();
+app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname+"/public"));
 
 ///////////////////////////////////////
 let posts =[];
+const port = process.env.PORT || 3000;
 
 
 ////////////////////////////////////////
@@ -79,7 +82,7 @@ app.post("/compose", function(req,res)
 });
 
 ////////////////////////////////////////
-app.listen(3000, function() {
+app.listen(port , function() {
    
   console.log("Server started on port 3000");
 });
